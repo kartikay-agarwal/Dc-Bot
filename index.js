2b9eb081252998a58fbc5da505689abd8c4e4ae9
kartikay-agarwal <73934694+kartikay-agarwal@users.noreply.github.com>
2025-05-26 00:52:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, Events, GatewayIntentBits, MessageFlags } = require('discord.js');
 const {token} = require('./config.json');
-
+require('dotenv').config();
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 // client.cooldowns = new Collection();
@@ -37,52 +37,6 @@ for (const file of eventFiles) {
 	}
 }
 
-// client.once(Events.ClientReady, readyClient => {
-// 	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
-// });
 
-// client.on(Events.InteractionCreate, async interaction => {
-// 	if (!interaction.isChatInputCommand()) return;
-// 	const command = client.commands.get(interaction.commandName);
 
-// 	if (!command) {
-// 		console.error(`No command matching ${interaction.commandName} was found.`);
-// 		return;
-// 	}
-
-// 	const { cooldowns } = interaction.client;
-
-// 	if (!cooldowns.has(command.data.name)) {
-// 		cooldowns.set(command.data.name, new Collection());
-// 	}
-
-// 	const now = Date.now();
-// 	const timestamps = cooldowns.get(command.data.name);
-// 	const defaultCooldownDuration = 3;
-// 	const cooldownAmount = (command.cooldown ?? defaultCooldownDuration) * 1000;
-
-// 	if (timestamps.has(interaction.user.id)) {
-// 		const expirationTime = timestamps.get(interaction.user.id) + cooldownAmount;
-
-// 		if (now < expirationTime) {
-// 			const expiredTimestamp = Math.round(expirationTime / 1000);
-// 			return interaction.reply({ content: `Please wait, you are on a cooldown for \`${command.data.name}\`. You can use it again <t:${expiredTimestamp}:R>.`, flags: MessageFlags.Ephemeral });
-// 		}
-// 	}
-
-// 	timestamps.set(interaction.user.id, now);
-// 	setTimeout(() => timestamps.delete(interaction.user.id), cooldownAmount);
-
-// 	try {
-// 		await command.execute(interaction);
-// 	} catch (error) {
-// 		console.error(error);
-// 		if (interaction.replied || interaction.deferred) {
-// 			await interaction.followUp({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
-// 		} else {
-// 			await interaction.reply({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
-// 		}
-// 	}
-// });
-
-client.login(token);
+client.login(process.env.token);
